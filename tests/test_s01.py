d2955b823ea78cd7c0aaf31dc90f07948188dd2c
palec87 <david@stanka.de>
2022-08-01 14:39:25 +0100

    try pytest

--- tests/test_s01.py
+++ tests/test_s01.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 import sys
 from pathlib import Path
 
@@ -6,18 +7,20 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 import scripts.s01_label_img_dfs_recursion as m
 
 
-def test_int_array():
+@pytest.mark.parametrize('mat, ass', [
+    (np.array([[1, 2, 3], [2, 2, 4], [7, 8, 9]]),
+     np.array([[-1, -1, 3], [-1, -1, 4], [7, 8, 9]])
+     ),
+    (np.array([[1, 1, 3], [2, 5, 4], [7, 1, 2]]),
+     np.array([[-1, -1, 3], [-1, 5, 4], [7, -2, -2]])
+     )
+    ])
+def test_int_array(mat, ass):
     '''
     test labeling simple array
     '''
     neighs = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-    mat = np.array([[1, 2, 3],
-                    [2, 2, 4],
-                    [7, 8, 9]])
     result = m.DFS(mat, 0, 0, neighs, th=3, label=-1, n=3, m=3)
-    ass = np.array([[-1, -1, 3],
-                    [-1, -1, 4],
-                    [7, 8, 9]])
     assert (result - ass).all() == 0
 
 
